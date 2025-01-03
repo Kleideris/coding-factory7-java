80a78b988ba121f1944cd9f70f4b8e5862862f55
Ch. Kleideris <kleiderisch@gmail.com>
2025-01-04 01:05:59 +0200

    Finished Project2

--- src/gr/aueb/cf/projects/Project2.java
+++ src/gr/aueb/cf/projects/Project2.java
@@ -2,23 +2,52 @@ package gr.aueb.cf.projects;
 
 /**
  * Calculates the maximum sum sub-array of the given array at time complexity O(n)
+ * (Also known as Kadane's Algorithm for the maximum sub-array problem)
  */
 public class Project2 {
 
     public static void main(String[] args) {
         int[] array = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        int subArrayTotal = 0;
-        int subArrayMax = 0;
-        int j = 0;
+        int localMaxSubArray = array[0];
+        int maxSubArray = array[0];
+        int subElements = 0;
+        int endIndex = 0;
 
-        for (int i = 0; i < array.length - j; i++) {
-            for (i = 0; i <= j; i++) {
-                subArrayTotal += array[i];
+        for (int i = 0; i < array.length; i++) {
+            int num = array[i];
+
+            if ((localMaxSubArray + num) < num) {
+                localMaxSubArray = num;
+                subElements = 0;
+            } else {
+                localMaxSubArray = localMaxSubArray + num;
             }
-            subArrayMax = Math.max(subArrayMax, subArrayTotal);
-            j++;
-            System.out.println(subArrayMax);
+
+            if (maxSubArray < localMaxSubArray) {
+                maxSubArray = localMaxSubArray;
+                endIndex = i;
+                subElements++;
+            }
         }
-        System.out.println(subArrayMax);
+
+        printSubArray(array, maxSubArray, endIndex, subElements);
+        printSubArrayMax(maxSubArray);
+    }
+
+    public static void printSubArray(int[] array, int maxSubArray, int endIndex, int subElements) {
+        System.out.print("Max Sub-array: {");
+        for (int i = endIndex - subElements; i <= endIndex; i++) {
+            System.out.print(array[i]);
+
+            if (i != endIndex) {
+                System.out.print(", ");
+            } else {
+                System.out.print("}");
+            }
+        }
+    }
+
+    public static void printSubArrayMax(int maxSubArray) {
+        System.out.println("   ,   Max Sub-array Sum: " + maxSubArray);
     }
 }
