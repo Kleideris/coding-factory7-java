c19bc81d8c6b12c2e2a503498458fec6c56ea4b7
Ch. Kleideris <kleiderisch@gmail.com>
2025-01-05 22:05:48 +0200

    Completed ch12 challenges

---
+++ src/gr/aueb/cf/model/Main.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.model;
+
+public class Main {
+
+    public static void main(String[] args) {
+        User chris = new User(25555, "chris", "kleideris");
+        UserCredentials adm = new UserCredentials(99999, "admin", "temp");
+        int a = 0;
+
+        System.out.println("User fields:  id = " + chris.getId() +
+                ", firstname = \"" + chris.getFirstname() + "\"" +
+                ", lastname = \"" + chris.getLastname() + "\"");
+
+        System.out.println("UserCredential fields:  id = " + adm.getId() +
+                ", firstname = \"" + adm.getFirstname() + "\"" +
+                ", lastname = \"" + adm.getLastname() + "\"");
+    }
+}
---
+++ src/gr/aueb/cf/model/User.java
@@ -0,0 +1,27 @@
+package gr.aueb.cf.model;
+
+public class User {
+    private long id;
+    private String firstname;
+    private String lastname;
+
+    //Default constructor
+    public User() {}
+
+    //Overloaded constructor
+    public User(long id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    //Getters and Setters
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
+
+    public String getFirstname() { return firstname;}
+    public void setFirstname(String firstname) { this.firstname = firstname;}
+
+    public String getLastname() { return lastname;}
+    public void setLastname(String lastname) { this.lastname = lastname;}
+}
