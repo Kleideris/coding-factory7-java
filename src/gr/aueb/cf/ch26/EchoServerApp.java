7b3d41fc2a90c1a299c90b212acc3fe7c36b2adc
Ch. Kleideris <kleiderisch@gmail.com>
2025-03-18 21:27:39 +0200

    hotfix: changed ch25 to ch26 inside the code

--- src/gr/aueb/cf/ch26/EchoServerApp.java
+++ src/gr/aueb/cf/ch26/EchoServerApp.java
@@ -1,9 +1,9 @@
-package gr.aueb.cf.ch25;
+package gr.aueb.cf.ch26;
 
 public class EchoServerApp {
 
     public static void main(String[] args) {
-        gr.aueb.cf.ch25.EchoServer echoServer = new gr.aueb.cf.ch25.EchoServer();
+        gr.aueb.cf.ch26.EchoServer echoServer = new gr.aueb.cf.ch26.EchoServer();
         echoServer.start();
     }
 }
