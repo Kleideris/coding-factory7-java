02762e8dd4c37a77edea2ec6061306c8ad7279fa
Ch. Kleideris <kleiderisch@gmail.com>
2025-03-18 21:27:01 +0200

    feat: crated classes ConcurrentDateTimeServer, ConcurrentDateTimeServerApp,  DateTimeCli,  DateTimeServer,  EchoCli,  EchoServer,  EchoServerApp,  IterativeDayTimeServer

---
+++ src/gr/aueb/cf/ch26/EchoServerApp.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.ch25;
+
+public class EchoServerApp {
+
+    public static void main(String[] args) {
+        gr.aueb.cf.ch25.EchoServer echoServer = new gr.aueb.cf.ch25.EchoServer();
+        echoServer.start();
+    }
+}
